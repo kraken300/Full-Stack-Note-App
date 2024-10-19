f38a3cedc80575162a46b044b662d364272a095d
kraken300 <shivamsingh113355@gmail.com>
2024-10-19 11:57:11 +0530

    Added cors in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
 const noteRoutes = require('./routes/noteRoutes');
@@ -10,6 +11,9 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'https://full-stack-note-app-frontend.onrender.com',
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
